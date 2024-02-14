ccc2ef5260e3b7c52d40096baa3184e36ab0733f
Adam Bujak <adamjbujak@gmail.com>
2024-02-14 12:44:11 -0800

    add profile feature

--- mac/install.sh
+++ mac/install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export PROFILE_PATH="${PROFILE_PATH:-profiles/adam}"
+if [ -z "$1" ]; then
+  echo "Usage: $0 <profile_name>"
+  echo "   ex: $0 adam"
+  exit 1
+fi
+
+PROFILE_PATH="profiles/$1"
+
+if [ ! -d "$PROFILE_PATH" ]; then
+  echo "Profile: "$1" not found!"
+  exit 1
+fi
 
 ./install_tools.sh
-./install_profile.sh
+./install_profile.sh $1
 
--- mac/install_profile.sh
+++ mac/install_profile.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-PROFILE_PATH="${PROFILE_PATH:-profiles/adam}"
+if [ -z "$1" ]; then
+  echo "Usage: $0 <profile_name>"
+  echo "   ex: $0 adam"
+  exit 1
+fi
+
+PROFILE_PATH="profiles/$1"
+
+if [ ! -d "$PROFILE_PATH" ]; then
+  echo "Profile: "$1" not found!"
+  exit 1
+fi
 
 # get home path
 pushd ~ > /dev/null
